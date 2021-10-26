75c17bb202e48bb4528e4d5d1c68286d7a3c9926
echoaj <aljoslin13@yahoo.com>
2021-10-25 18:22:45 -0700

    New Branch created

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Welcome to Task Guru.</h1>"
+    return "<h1>Welcome to Task Guru  --version Final.</h1>"
 
 
 if __name__ == '__main__':
