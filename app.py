6f3f8e35820bb85b5af4350447830f671d56fc75
echoaj <aljoslin13@yahoo.com>
2021-10-25 18:49:54 -0700

    made by alex

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Welcome to Task Guru.</h1>"
+    return "<h1>Welcome to Task Guru Made by Alex.</h1>"
 
 
 if __name__ == '__main__':
